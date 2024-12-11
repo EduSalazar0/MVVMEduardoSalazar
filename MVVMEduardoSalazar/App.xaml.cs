d16865296b7cf3a93e6fe7e5b24d377b85acd8b7
EduSalazar0 <edutebansalazar95@gmail.com>
2024-12-11 07:24:19 -0500

    Agregar archivos de proyecto.

---
+++ MVVMEduardoSalazar/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MVVMEduardoSalazar
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MVVMEduardoSalazar/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MVVMEduardoSalazar
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
